430b8cbdd16e590753ec90d719e2ba17911e6df6
wellusion <wellusionmedia@gmail.com>
2021-03-12 23:43:26 +0500

    Mobile ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const body = document.body,
+      header = document.querySelector('.header'),
+      headerBtns = document.querySelectorAll('.hamburger, .helper, .close-menu');
+
+headerBtns.forEach(function(btn){
+  btn.addEventListener('click', function(){
+    body.classList.toggle('overflowed');
+    header.classList.toggle('active');
+  });
+});
+
 const feedbackSlider = new Swiper('.feedback-slider', {
   spaceBetween: 120,
   pagination: {
